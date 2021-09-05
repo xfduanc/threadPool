8cca08c09c3675216e6534b5843bc9cdecbd5484
xfduanc <pickmeup4536@163.com>
2021-09-05 17:59:25 +0800

    线程池实现部分

--- ThreadPool.h
+++ ThreadPool.h
@@ -14,7 +14,7 @@
 class ThreadPool {
 public:
     ThreadPool(size_t);
-    template<class F, class... Args>
+    template<typename F, typename... Args>
     auto enqueue(F&& f, Args&&... args) 
         -> std::future<typename std::result_of<F(Args...)>::type>;
     ~ThreadPool();
@@ -36,41 +36,54 @@ inline ThreadPool::ThreadPool(size_t threads)
 {
     for(size_t i = 0;i<threads;++i)
         workers.emplace_back(
-            [this]
+            [this]()
             {
-                for(;;)
+                //这个task就是一个壳子，啥也没有，里面调用了packaged_task()调用了operator()
+                std::function<void()> task;
+                while(true)
                 {
-                    std::function<void()> task;
-
                     {
+                    /*
+                        template <class Predicate>
+                        void wait (unique_lock<mutex>& lck, Predicate pred);
+                        if pred false , block, true can through
+                    */
+                   //只有条件为true才能穿过
                         std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
+                        this->condition.wait(lock,\
+                                 [this]{ return this->stop || !this->tasks.empty(); });
                         if(this->stop && this->tasks.empty())
                             return;
                         task = std::move(this->tasks.front());
                         this->tasks.pop();
                     }
-
                     task();
+                    //取到了任务，执行任务，不要return ，继续监听这个任务队列
                 }
-            }
-        );
+            }//end of thread body
+        );//end of emplace_back
 }
-
-// add new work item to the pool
-template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
+/*
+template <typename  Fn, typename... Args>
+struct result_of<Fn(Args...)>;
+*/
+// add new work_task into the queue of the ThreadPool
+// retT is std::future< std::result_of<Fn(Args)...>>
+template<class Fn, class... Args>
+auto ThreadPool::enqueue( Fn&& f, Args&&... args) 
+    -> std::future< typename std::result_of<Fn(Args...)>::type >
 {
-    using return_type = typename std::result_of<F(Args...)>::type;
+    using return_type = typename std::result_of<Fn(Args...)>::type;
 
+    //return_type为future的模板类型，也就是返回值future的模板类型
+    //通过接收端的bind来绑定参数，在运行端去设置具体的参数
     auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+            std::bind(std::forward<Fn>(f), std::forward<Args>(args)...)
         );
-        
+
     std::future<return_type> res = task->get_future();
     {
+        //在独占式锁这个地方去插入线程的入口，注意这个线程的入口是通过packaged_task的operator()来实现的
         std::unique_lock<std::mutex> lock(queue_mutex);
 
         // don't allow enqueueing after stopping the pool
@@ -79,7 +92,9 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
 
         tasks.emplace([task](){ (*task)(); });
     }
+    //结束一次插入，条件变量+1
     condition.notify_one();
+    //返回这个future，这个future是通过packaged_task的get_future来实现的
     return res;
 }
 
--- example.cpp
+++ example.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <type_traits>
+#include <limits.h>
+#include <unistd.h>
+#include <atomic>
 
 #include "ThreadPool.h"
 
+auto func = [](int k){for(int j =2; j<k;++j)\
+                            if( k%j ==0)return false;
+                            return true;};
+// auto increse = [&inc](){}
 int main()
 {
-    
-    ThreadPool pool(4);
-    std::vector< std::future<int> > results;
-
-    for(int i = 0; i < 8; ++i) {
-        results.emplace_back(
-            pool.enqueue([i] {
-                std::cout << "hello " << i << std::endl;
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::cout << "world " << i << std::endl;
-                return i*i;
-            })
-        );
+    char buf[1024];
+    ::read(STDIN_FILENO, buf, 1024);
+    int cpuNumber = sysconf(_SC_NPROCESSORS_CONF);
+    ThreadPool pool(cpuNumber);
+    //保存异步任务结果的vector
+    std::vector<std::future<void> > results;
+    //ThreadPool.enqueue( [](){;} )    typename std::result_of<Fn(Args...)>::type
+    std::atomic<int> inc(0);
+    for(int i = 0; i < 100000; ++i) {
+        // results.emplace_back( pool.enqueue([&inc](){ ++inc;}) );
+        pool.enqueue([&inc](){ ++inc;});
     }
-
-    for(auto && result: results)
-        std::cout << result.get() << ' ';
-    std::cout << std::endl;
+    // int ret=0;
+    // for(auto && result: results)
+    //     if(result.get())ret++;
+    std::cout << static_cast<int>(inc.load())<< std::endl;
     
     return 0;
 }
